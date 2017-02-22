7b89ab610f417feca14ca37c497262ace1a6fdc6
Paul Danilin <pidanilin@edu.hse.ru>
2017-02-22 15:57:30 +0300

    Added tests and author comments.

--- BidiLinkedList/BidiLinkedList_Student_Test.cpp
+++ BidiLinkedList/BidiLinkedList_Student_Test.cpp
@@ -10,11 +10,13 @@
 typedef BidiLinkedList<int> IntBidiList;
 typedef IntBidiList::Node IntBidiListNode;
 
+// Paul Danilin
 TEST(StudentBidiListInt, lastNode1) {
     IntBidiList list;
     EXPECT_EQ(list.getLastNode(), nullptr);
 }
 
+// Paul Danilin
 TEST(StudentBidiListInt, insertNodesAfter1) {
     IntBidiList list;
     list.appendEl(1);
@@ -86,6 +88,7 @@ TEST(StudentBidiListInt, insertNodesAfter1) {
     EXPECT_EQ(start->getNext()->getNext()->getNext()->getNext()->getValue(), 3);
 }
 
+// Paul Danilin
 TEST(StudentBidiListInt, clear1) {
     IntBidiList list;
     list.appendEl(1);
@@ -96,4 +99,80 @@ TEST(StudentBidiListInt, clear1) {
 
     EXPECT_EQ(list.getSize(), 0);
     EXPECT_EQ(list.getHeadNode(), nullptr);
+}
+
+// Andrey Morozov
+TEST(StudentBidiListInt, insNodesAfter0)
+{
+    IntBidiList lst;
+
+    IntBidiListNode* nd0 =lst.appendEl(1);
+    IntBidiListNode* nd1 = lst.appendEl(2);
+    IntBidiListNode* beg = lst.appendEl(3);
+    lst.appendEl(4);
+    IntBidiListNode* end = lst.appendEl(5);
+
+    lst.cutNodes(beg, end);
+    EXPECT_EQ(lst.getLastNode(), nd1);
+    EXPECT_EQ(lst.getSize(), 2);
+
+    lst.insertNodesAfter(nd1, beg, end);
+    EXPECT_EQ(lst.getLastNode(), end);
+    EXPECT_EQ(lst.getSize(), 5);
+
+    lst.cutNodes(nd0, end);
+    EXPECT_EQ(lst.getHeadNode(), nullptr);
+    EXPECT_EQ(lst.getLastNode(), nullptr);
+    EXPECT_EQ(lst.getSize(), 0);
+
+    lst.insertNodesAfter(nullptr, nd0, end);
+    EXPECT_EQ(lst.getLastNode(), end);
+    EXPECT_EQ(lst.getHeadNode(), nd0);
+    EXPECT_EQ(lst.getSize(), 5);
+}
+
+// Andrey Morozov
+TEST(StudentBidiListInt, Cut)
+{
+    IntBidiList lst;
+
+    EXPECT_THROW(lst.cutNode(nullptr), std::invalid_argument);
+
+    IntBidiListNode* beg = lst.appendEl(10);
+    IntBidiListNode* sec =lst.appendEl(10);
+    lst.appendEl(10);
+    lst.appendEl(10);
+    lst.appendEl(10);
+
+    lst.cutNode(beg);
+    EXPECT_EQ(lst.getSize(), 4);
+    EXPECT_EQ(lst.getHeadNode(), sec);
+    EXPECT_EQ(sec->getPrev(), nullptr);
+
+    int s;
+    lst.cutAll(sec, 10, s);
+    EXPECT_EQ(lst.getLastNode(), nullptr);
+    EXPECT_EQ(lst.getHeadNode(), nullptr);
+    EXPECT_EQ(s, 4);
+    EXPECT_EQ(lst.getSize(), 0);
+}
+
+// Andrey Morozov
+TEST(StudentBidiListInt, InsBef)
+{
+    IntBidiList lst;
+
+    IntBidiListNode* nd = new IntBidiListNode(10);
+    lst.insertNodeBefore(nullptr, nd);
+
+    EXPECT_EQ(lst.getHeadNode(), nd);
+    EXPECT_EQ(lst.getLastNode(), nd);
+    EXPECT_EQ(lst.getSize(), 1);
+
+    IntBidiListNode* nd1 = new IntBidiListNode(20);
+    lst.insertNodesBefore(nd, nd1, nd1);
+
+    EXPECT_EQ(lst.getHeadNode(), nd1);
+    EXPECT_EQ(lst.getLastNode(), nd);
+    EXPECT_EQ(lst.getSize(), 2);
 }
